27ee6531e12f2fbcfea1553d178d7807d85fadf0
subbus <subbu4communicate@gmail.com>
2023-08-30 22:50:11 +0530

    React: Removed main

--- reactcdn/app.js
+++ reactcdn/app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 (function(){
     let heading = React.createElement("h1",{},"Hello World With React");
